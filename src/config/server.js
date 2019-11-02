f9a73604b0108222c58bd6a4db1c1111a65db2ad
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2019-11-02 11:21:22 -0300

    base express

---
+++ src/config/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+const port = process.env.port || 3003
+server.listen(
+  port,
+  function() {
+    console.log(`Todo-list Backend is running on port ${port}`);
+  }
+);
